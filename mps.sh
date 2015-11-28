6d3d8ec0fccf498ba41f8c0679d65f72bc730b5a
ramakantmoka <ramakantmoka@gmail.com>
2015-11-28 10:54:32 -0500

    autostart

---
+++ mps.sh
@@ -0,0 +1,10 @@
+[Desktop Entry]
+Type=Application
+Exec="/home/vagrant/Desktop/MPS 3.2/mps.sh"
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=MPS
+Name=MPS
+Comment[en_US]=TRY
+Comment=TRY
