323e2d1dfbb1953bac57622fb23a0fff4510fd9e
ramakantmoka <ramakantmoka@gmail.com>
2015-11-28 19:26:25 -0500

    Update screenlock.sh

--- screenlock.sh
+++ screenlock.sh
@@ -1,3 +1,6 @@
-#! /bin/bash
+#!/bin/sh
 /usr/bin/gsettings set org.gnome.desktop.lockdown disable-lock-screen true
+/usr/bin/gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 /usr/bin/gsettings set org.gnome.desktop.screensaver lock-enabled false
+/usr/bin/gsettings set com.canonical.Unity.Launcher favorites "['application://nautilus-home.desktop', 'application://firefox.desktop', 'unity://running-apps', 'unity://devices']"
+
